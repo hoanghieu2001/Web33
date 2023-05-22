e9ec398e2a68e79c828b69a2dc808f161f76ac93
hoanghieu2001 <104782203+hoanghieu2001@users.noreply.github.com>
2023-05-22 08:36:47 +0700

    22-5-2023

--- angular/lab1/src/app/app.module.ts
+++ angular/lab1/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
